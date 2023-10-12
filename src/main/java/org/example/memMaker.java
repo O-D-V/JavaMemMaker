fa255d39746a808533d6940507a9102aaf437acc
Dmitry Ozn <dmitry712543@gmail.com>
2023-10-12 16:41:14 +0300

    version 1.0 with comments

--- src/main/java/org/example/memMaker.java
+++ src/main/java/org/example/memMaker.java
@@ -12,11 +12,9 @@ public class memMaker {
     private String[] args;
     private ArrayList<String> commandsDescriptionsList;
     private float textPosition;
-
     private int fontSize;
 
     public memMaker(String[] args) {
-
         commandsDescriptionsList = new ArrayList<>();
         commandsDescriptionsList.add("mem - create a meme with your picture and text. args:(imagePath, text[, textPosition, fontSize]).\n" +
                 "Example: mem image.jpg memtext textposition:center fontsize:25 \n "+
@@ -24,6 +22,7 @@ public class memMaker {
         this.args = args;
     }
 
+    //main processing func
     public void argsHandling() {
         for(int i = 0; i < args.length; i++){
             System.out.println(args[i]);
@@ -33,19 +32,21 @@ public class memMaker {
                 printHelp(commandsDescriptionsList);
             }
             else if(args[0].equals("mem")){
-                argsConventer();
-                createMeme();
+                argsConvent();
+                createAMeme();
             }
         }
     }
 
-    public void argsConventer(){
+    //arguments are converted to variables
+    public void argsConvent(){
         textPosition = 0.8f;
         fontSize = 25;
         Map<String, Float> textPositions = new HashMap<String, Float>();
         textPositions.put("bottom", 0.8f);
         textPositions.put("center", 0.5f);
         textPositions.put("top", 0.2f);
+
         for (String arg:args){
 
             if(arg.toLowerCase().indexOf("textposition:") != -1){
@@ -58,14 +59,15 @@ public class memMaker {
 
     }
 
-    public void createMeme() {
+    //put text on image
+    public void createAMeme() {
         BufferedImage image = new BufferedImage(256, 256, BufferedImage.TYPE_INT_RGB);
         try {
             image = ImageIO.read(new File(args[1]));
         }catch (IOException e){
             System.out.println("Image open error");
             e.printStackTrace();
-            //return;
+            return;
         }
         Graphics g = image.createGraphics();
         g.setColor(Color.WHITE);
@@ -73,12 +75,13 @@ public class memMaker {
         g.drawString(args[2], image.getWidth()/3, (int) (image.getHeight() * textPosition));
         g.dispose();
         try {
-            ImageIO.write(image, "png", new File("result.png"));//заменить pathname на args[1] чтобы заменять картинку, а не создавать новую
+            ImageIO.write(image, "png", new File("result.png"));
         }catch (IOException e){
             System.out.println("Image save error");
         }
     }
 
+    //print help to console
     public void printHelp(ArrayList<String> commandsDescriptionsList) {
         for (String command : commandsDescriptionsList) {
             System.out.println(command);
